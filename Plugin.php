5d50b777b516e72720adcdcf0bc69a712d31c006
TohurTV <joshwebb84@outlook.com>
2020-07-06 19:14:03 -0600

    add access token refreshing logic

--- Plugin.php
+++ Plugin.php
@@ -11,8 +11,11 @@ use Illuminate\Foundation\AliasLoader;
 use System\Classes\PluginBase;
 use System\Classes\SettingsManager;
 use October\Rain\Support\Collection;
+use October\Rain\Exception\ApplicationException;
 use Backend\Widgets\Form;
+use Carbon\Carbon;
 use Tohur\SocialConnect\Classes\Apis\TwitchAPI;
+use Tohur\SocialConnect\Models\TwitchApptokens;
 
 class Plugin extends PluginBase {
 
@@ -23,7 +26,7 @@ class Plugin extends PluginBase {
     public function pluginDetails() {
         return [
             'name' => 'Twitch Intergration',
-            'description' => 'tohur.twitchintergration::lang.plugin.description',
+            'description' => 'Provides Twitch.tv integration services.',
             'author' => 'Joshua Webb',
             'icon' => 'icon-video-camera'
         ];
@@ -43,15 +46,22 @@ class Plugin extends PluginBase {
             $twitch = new TwitchAPI();
             $twitchAPISettings = \Tohur\SocialConnect\Models\Settings::instance()->get('providers', []);
             if (!strlen($twitchAPISettings['Twitch']['client_id']))
-                throw new ApplicationException('Twitter API access is not configured. Please configure it on the Social Connect Settings Twitter tab.');
+                throw new ApplicationException('Twitch API access is not configured. Please configure it on the Social Connect Settings Twitch tab.');
             $client_id = $twitchAPISettings['Twitch']['client_id'];
             $client_secret = $twitchAPISettings['Twitch']['client_secret'];
-            $tokenRequest = json_decode($twitch->helixTokenRequest($twitch->oAuthbaseUrl . "?client_id=" . $client_id . "&client_secret=" . $client_secret . "&grant_type=client_credentials&scope=channel:read:hype_train%20channel:read:subscriptions%20bits:read%20user:read:broadcast%20user:read:email"), true);
-            $accessToken = $tokenRequest['access_token'];
-            $tokenExpires = $tokenRequest['expires_in'];
-            \Db::table('tohur_twitchintergration_apptokens')
-                    ->where('id', 1)
-                    ->update(['access_token' => $accessToken, 'expires_in' => $tokenExpires, 'updated_at' => now()]);
+            $tokens = \DB::select('select * from tohur_socialconnect_twitch_apptokens where id = ?', array(1));
+            $expiresIn = $tokens[0]->expires_in;
+            $current = Carbon::now();
+            $expired = Carbon::parse($tokens[0]->updated_at)->addSeconds($expiresIn);
+
+            if ($current > $expired) {
+                $tokenRequest = json_decode($twitch->helixTokenRequest($twitch->oAuthbaseUrl . "?client_id=" . $client_id . "&client_secret=" . $client_secret . "&grant_type=refresh_token&scope=channel:read:hype_train%20channel:read:subscriptions%20bits:read%20user:read:broadcast%20user:read:email"), true);
+                $accessToken = $tokenRequest['access_token'];
+                $tokenExpires = $expiresIn;
+                \Db::table('tohur_socialconnect_twitch_apptokens')
+                        ->where('id', 1)
+                        ->update(['access_token' => $accessToken, 'expires_in' => $tokenExpires, 'updated_at' => now()]);
+            }
         })->weekly();
     }
 
